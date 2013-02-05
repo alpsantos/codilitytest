d663f47625ba73d4fdd1843894cb443a0f61d601
Alessandro Santos <alpsantos>
2013-02-05 09:44:10 -0200

    first code/tests... maybe v0.00001

---
+++ CodilityTest.Tests/UnitTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityTest.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void deve_retornar_5_como_quantidade_de_distintos()
+        {
+            var A = new int[6];
+            A[0] = -5;
+            A[1] = -3;
+            A[2] = -1;
+            A[3] = 0;
+            A[4] = 3;
+            A[5] = 6;
+
+            var ret = Test.absDistinct(A);
+
+            Assert.AreEqual(5, ret);
+        }
+
+        [TestMethod]
+        public void deve_retornar_0_como_quantidade_de_distintos()
+        {
+            var A = new int[2];
+            A[0] = 0;
+            A[1] = -100;
+
+            var ret = Test.absDistinct(A);
+            Assert.AreEqual(0, ret);
+        }
+
+        [TestMethod]
+        public void deve_retornar_0_como_menor_distancia()
+        {
+            var A = new int[8];
+            A[0] = 0;
+            A[1] = 3;
+            A[2] = 3;
+            A[3] = 7;
+            A[4] = 5;
+            A[5] = 3;
+            A[6] = 11;
+            A[7] = 1;
+
+            var ret = Test.adjacent_points_min_distance(A);
+            Assert.AreEqual(0, ret);
+
+        }
+    }
+}
---
+++ CodilityTest/Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace CodilityTest
+{
+    public class Test
+    {
+        public static int adjacent_points_min_distance ( int[] A )
+        {
+            var total_indices = A.Length -1;
+            var indice_referencia_esquerda = 0;
+            var menor_distancia_encontrada = -2;
+
+            while (indice_referencia_esquerda <= total_indices)
+            {
+
+                for (int i = indice_referencia_esquerda; i < total_indices; i++)
+                {
+                    if (A[indice_referencia_esquerda] == A[i + 1])
+                    {
+                        menor_distancia_encontrada = 0;
+                        continue;
+                    }
+
+                    if (Math.Abs(A[indice_referencia_esquerda] - A[i]) < menor_distancia_encontrada)
+                    {
+                        menor_distancia_encontrada = A[indice_referencia_esquerda] - A[i];
+                    }
+                }
+
+                indice_referencia_esquerda++;
+            }
+
+            if (menor_distancia_encontrada > 100000000)
+                return -1;
+            return menor_distancia_encontrada;
+        }
+
+        public static int absDistinct(int[] A)
+        {
+            var ocorrencias = 0;
+            var indiceEsquerda = 0;
+            var indiceDireta = A.Length - 1;
+            while (indiceEsquerda < indiceDireta)
+            {
+                if (Math.Abs(A[indiceEsquerda]) == Math.Abs(A[indiceEsquerda + 1]))
+                {
+                    ocorrencias++;
+                    indiceEsquerda++;
+                    continue;
+                }
+
+                if (A[indiceEsquerda] < 0)
+                {
+                    for (int count = indiceDireta; Math.Abs(A[indiceEsquerda]) <= Math.Abs(A[count]); count--)
+                    {
+                        if (Math.Abs(A[indiceEsquerda]) == Math.Abs(A[count]))
+                        {
+                            ocorrencias++;
+                        }
+                    }
+                }
+                indiceEsquerda++;
+            }
+            if (ocorrencias == 0)
+                return 0;
+
+            return A.Length - ocorrencias;
+        }
+    }
+}
